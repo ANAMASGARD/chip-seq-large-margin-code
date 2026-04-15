f1f2c47dec79afb24a242f0b7ad1935d69944b91
Gaurav Chaudhary <chaudharygaurav2004@gmail.com>
2026-04-15 22:00:13 +0530

    Adjusted blue label positions to avoid overlap

--- chip-seq-large-margin.R
+++ chip-seq-large-margin.R
@@ -315,13 +315,13 @@ viz <- animint(
     theme_animint(width=800, height=500)+
     facet_grid(. ~ what, scales="free")+
     ggtitle("Select sample and number of peaks")+
-    geom_text(aes(feature, log.penalty, label=label, hjust=hjust),
+    geom_text(aes(x, log.penalty, label=label, hjust=hjust),
               data=data.table(
                 what="regression",
-                feature=c(5.8, 6.99, 6.75, 5.8),
-                log.penalty=c(12.8, 11.0, 10.76, 9.0),
-                hjust=c(0.5, 1, 0.5, 0),
-                label=c("0 errors\nlarge margin", "0 errors\nsmall", "margin", "1 error\nconstant")),
+                x=c(6.2, Inf, Inf),
+                log.penalty=c(13.0, 10.2, 9.0),
+                hjust=c(1, 1, 1),
+                label=c("0 errors\nlarge margin", "0 errors\nsmall\nmargin", "1 error\nconstant")),
               color="blue", size=12)+
     # Target intervals (clickable) - thick segment for easier clicking
     geom_segment(aes(log.max.count, min.log.lambda,
